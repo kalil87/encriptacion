b99f87c847eafb97844b78f6732697214477e62f
Kalil Sat <kalilomarsat@gmail.com>
2026-05-06 23:47:21 -0300

    chore: configurar conexión a base de datos

--- src/main/java/com/proyecto/encriptacion/entity/Producto.java
+++ src/main/java/com/proyecto/encriptacion/entity/Producto.java
@@ -15,7 +15,7 @@ import lombok.Setter;
 public class Producto {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable=false, length = 100)
